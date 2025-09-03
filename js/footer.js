cb95ea40eff0fa1f74117353b10df01dd58c000e
Daniel Morrisey <142274379+therealfuntimeswithdanny@users.noreply.github.com>
2025-09-03 17:32:26 -0400

    css fixes

--- js/footer.js
+++ js/footer.js
@@ -6,9 +6,10 @@ function initFooter() {
   footer.innerHTML = `
     <p>
       &copy; 2024-<span id="current-year"></span> Made by Danny UK,
-      <i>by Daniel Morrisey</i> / <a><i class="fa-solid fa-magnifying-glass"></i></a>
+      <i>by Daniel Morrisey</i> / <a><i class="fa-solid fa-magnifying-glass"></i></a> /
       <button id="theme-toggle">Switch to Light Mode</button>
     </p>
+    <iframe src="https://status.madebydanny.uk/badge?theme=dark" width="200" height="24" frameborder="0" scrolling="no" style="color-scheme: normal"></iframe>
   `;
 
   const yearEl = document.getElementById('current-year');
